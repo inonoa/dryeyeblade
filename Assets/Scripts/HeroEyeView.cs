f3278c2bff835ef0e6d989c5965c40eb9374a5ff
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-12-30 04:51:16 +0900

    ポストプロセスマシマシ

--- Assets/Scripts/HeroEyeView.cs
+++ Assets/Scripts/HeroEyeView.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Linq;
 using DG.Tweening;
 using UniRx;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
 public class HeroEyeView : MonoBehaviour
 {
     [SerializeField] Animator blindAnim;
+    [SerializeField] PostProcessVolume postProcess;
+    [SerializeField] float bloomIntensityInClosing = 20;
+    [SerializeField] float bloomIntensityInOpening = 10;
     
     Tween openOrCloseCurrent;
+    Bloom bloom;
 
     void Awake()
     {
+        bloom = postProcess.profile.settings.First(setting => setting is Bloom) as Bloom;
+        bloom.intensity.value = bloomIntensityInOpening;
+        
         Hero.Current
             .Where(hero => hero != null)
             .Subscribe(hero =>
@@ -26,6 +35,14 @@ public class HeroEyeView : MonoBehaviour
                     (
                         blindAnim.GetCurrentAnimatorStateInfo(0).length,
                         () => blindAnim.enabled = false
+                    );
+
+                    DOTween.To
+                    (
+                        ()  => bloom.intensity.value,
+                        val => bloom.intensity.value = val,
+                        open ? bloomIntensityInOpening : bloomIntensityInClosing,
+                        blindAnim.GetCurrentAnimatorStateInfo(0).length / 2
                     );
                 })
                 .AddTo(this);
